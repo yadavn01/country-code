5b77cb19c01f6bb54477f93b861a9fe13dd57711
yadavn01 <ynaman29@gmail.com>
2024-02-04 08:54:00 +1300

    buttons showing for each country and capital

--- src/App.js
+++ src/App.js
@@ -6,35 +6,8 @@ import CountryCapital from './components/CountryCapital';
 
 function App() {
 
-  // const [buttonColor , setButtonColor] = useState('red');
-
-  const countryData = {
-    "USA" : "Washington DC",
-    "New Zealand" : "Wellington",
-    "Nepal" : "Kathmandu",
-    "France": "Paris",
-  };
-  const countryNames = Object.keys(countryData);
-
-  const handleButtonClick = (country) => {
-    // Handle button click here
-    console.log(`${country}'s capital is ${countryData[country]}`);
-  };
-
-  // var changeColor = () => {
-  //   const newColor = buttonColor === 'red' ? 'blue' : 'red';
-  //   setButtonColor(newColor);
-  // }
-
   return (
-   <div className="main">
-    <div className="leftPane">
-        <CountryCapital countryNames={countryNames} onButtonClick={handleButtonClick}/>
-    </div>
-    <div className="rightPane">
-      
-    </div>
-   </div>
+    <CountryCapital ></CountryCapital>
   );
 }
 
--- src/components/CountryCapital.jsx
+++ src/components/CountryCapital.jsx
@@ -1,15 +1,45 @@
-import Button from './components/Button';
+// import Button from './components/Button';
 
+const CountryCapital = () => {
 
-const CountryCapital = ({ countryNames, onButtonClick }) => {
+    const countryData = {
+        "USA" : "Washington DC",
+        "New Zealand" : "Wellington",
+        "Nepal" : "Kathmandu",
+        "France": "Paris",
+      };
+      const countryNames = Object.keys(countryData);
+      const countryCapitals = Object.values(countryData);
+      const options = [...countryNames, ...countryCapitals];
+    
+      const handleButtonClick = (country) => {
+        // Handle button click here
+        console.log(`${countryNames}'s capital is ${countryCapitals}`);
+      };
 
     return (
         <>
+
+<div className="main">
+    <div className="leftPane">
         {countryNames.map((country) => (
-              <Button key={country} style={{ margin: '5px' }} onClick={() => onButtonClick(country)}>
+         <button key={country} style={{ margin: '5px' }} onClick={handleButtonClick}>
                 {country}
-              </Button>
+              </button>
         ))}
+        {/* <CountryCapital countryNames={countryNames} onButtonClick={handleButtonClick}/> */}
+    </div>
+    <div className="rightPane">
+    {countryCapitals.map((capitals) => (
+         <button key={capitals} style={{ margin: '5px' }} onClick={handleButtonClick}>
+                {capitals}
+              </button>
+        ))}
+      
+    </div>
+   </div>
+   
+
         </>
     );
   };
