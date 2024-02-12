4b6ec451269e14fbe390d284c90b14b69ad9c25b
yadavn01 <ynaman29@gmail.com>
2024-02-13 00:18:39 +1300

    fixing button random order bug and changing colurs

--- src/components/CountryCapital.jsx
+++ src/components/CountryCapital.jsx
@@ -2,10 +2,7 @@ import { useState } from 'react';
 
 // let buttonState = "DEFAULT" | "SELECTED" | "WRONG";
 // const blueColor = "blue";
-interface Option {
-    value: string;
-    state: string;
-}
+
 
 const CountryCapital = () => {
     const [colour, setColour] = useState({});
@@ -16,42 +13,57 @@ const CountryCapital = () => {
         "Nepal" : "Kathmandu",
         "France": "Paris",
       };
+      
       const countryNames = Object.keys(countryData);
       const countryCapitals = Object.values(countryData);
       //const options = [...countryNames, ...countryCapitals];
       const [options , setOptions] = useState([...countryNames, ...countryCapitals].map((value) => ({
+        label: value,
         value,
         state: 'DEFAULT',
 
       })))
       options.sort(() => Math.random() - 0.5);
 
-
-      const handleButtonClick = (option) => {
-        // Handle button click here
+        const handleButtonClick = (option) => {
+            // Handle button click here
             // Determine if the clicked option is correct or incorrect
-    const isCorrect = countryNames.includes(option) && countryData[option] === countryCapitals[countryNames.indexOf(option)];
-
-        setColour(prevState => ({
-            ...prevState,
-            [option] : "SELECTED",
-        }))};
+            // const isCorrect = countryNames.includes(option) && countryData[option] === countryCapitals[countryNames.indexOf(option)];
+          
+            setOptions((prevOptions) => {
+              return prevOptions.map((prevOption) => {
+                if (prevOption.value === option) {
+                  return { ...prevOption, state: 'SELECTED' };
+                }
+                return prevOption;
+              });
+            });
+          };
 
     return (
         <>
 <div className="main">
     <div className="leftPane">
         {options.map((option) => (
-         <button  style={{
+         <button 
+         key={option.value}
+         className={option.state === 'SELECTED' ? 'SELECTED' : "" }
+          style={{
             margin: '5px',
-            backgroundColor: colour[option] === "SELECTED" ? 'blue' : 'initial'
-          }} onClick={()=> {handleButtonClick(option)}}>
-                {option}
+            backgroundColor:
+        option.state === 'SELECTED'
+          ? 'blue'
+          : countryData[option.value] === option.label
+          ? 'green'
+          : 'red',
+          }} 
+          onClick={()=> {handleButtonClick(option.value)}}>
+                {option.label}
               </button>
         ))}
     </div>
 
-    <div className="rightPane">
+    {/* <div className="rightPane">
     {options.map((option) => (
          <button  style={{
             margin: '5px',
@@ -61,7 +73,7 @@ const CountryCapital = () => {
               </button>
         ))}
       
-    </div>
+    </div> */}
    </div>
         </>
     );
